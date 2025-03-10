a063217293be9c1183a1e73f5896a0479e175675
eunoia73 <soo97703@naver.com>
2025-03-10 11:17:10 +0900

    refactor: OCP(col, row 확장 가능하도록 변경)

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -2,7 +2,9 @@ package cleancode.minesweeper.tobe.io;
 
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
-import cleancode.minesweeper.tobe.Cell;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class ConsoleOutputHandler {
 
@@ -14,7 +16,9 @@ public class ConsoleOutputHandler {
     }
 
     public void showBoard(GameBoard board) {
-        System.out.println("   a b c d e f g h i j");
+        String alphabets = generateColAlphabets(board);
+
+        System.out.println("   " + alphabets);
         for (int row = 0; row < board.getRowSize(); row++) {
             System.out.printf("%d  ", row + 1);
             for (int col = 0; col < board.getColSize(); col++) {
@@ -23,6 +27,14 @@ public class ConsoleOutputHandler {
             System.out.println();
         }
         System.out.println();
+    }
+
+    private String generateColAlphabets(GameBoard board) {
+        List<String> alphabets = IntStream.range(0, board.getColSize())
+                .mapToObj(index -> (char) ('a' + index))
+                .map(Object::toString)
+                .toList();
+        return String.join(" ", alphabets);
     }
 
     public void printGameWinningComment() {
