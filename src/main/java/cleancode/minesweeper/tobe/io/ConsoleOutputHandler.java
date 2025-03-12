6bee21a4ad80d6b2789684c36879e85e0060ca93
eunoia73 <soo97703@naver.com>
2025-03-12 21:13:45 +0900

    refactor: Value Object 생성(CellPosition, RelativePosition)

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -2,6 +2,7 @@ package cleancode.minesweeper.tobe.io;
 
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
+import cleancode.minesweeper.tobe.position.CellPosition;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -23,7 +24,8 @@ public class ConsoleOutputHandler implements OutputHandler {
         for (int row = 0; row < board.getRowSize(); row++) {
             System.out.printf("%2d  ", row + 1);
             for (int col = 0; col < board.getColSize(); col++) {
-                System.out.print(board.getSign(row, col) + " ");  //보드를 그리는 부분 -> cell에서 정보를 받아와서 그려야 함
+                CellPosition cellPosition = CellPosition.of(row, col);
+                System.out.print(board.getSign(cellPosition) + " ");  //보드를 그리는 부분 -> cell에서 정보를 받아와서 그려야 함
             }
             System.out.println();
         }
