aa6d6370f69eede27f27bf4b283a72531a05bd0f
eunoia73 <soo97703@naver.com>
2025-03-12 21:42:47 +0900

    feat: first commit

---
+++ src/main/java/cleancode/studycafe/mission3/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.mission3;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/cleancode/studycafe/mission3/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.mission3.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
